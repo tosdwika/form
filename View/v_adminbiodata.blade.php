68011c30d0bca1ba10becb3d9a027be2a9da5930
tosdwika <tosdwika@gmail.com>
2023-02-16 08:54:30 +0700

    Add files via upload

---
+++ View/v_adminbiodata.blade.php
@@ -0,0 +1,64 @@
+@extends('ui.dashboard')
+@section('title','Admin')
+@section('content')
+@if (session('pesan'))
+<div class="alert alert-success alert-dismissible">
+  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+  {{ session('pesan') }}
+</div>
+@endif
+<table class="table table-bordered">
+  <thead>
+    <tr>
+      <th>No</th>
+      <th>Nama</th>
+      <th>Tempat & Tanggal Lahir</th>
+      <th>Posisi Yang Dilamar</th>
+      <th>Aksi</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php $no = 1; ?>
+    @foreach ($biodata as $data)
+    <tr>
+      <td>{{ $no++ }}</td>
+      <td>{{ $data->nama }}</td>
+      <td>{{ $data->tempat_lahir }}, {{ \Carbon\Carbon::parse($data->tanggal_lahir)->translatedFormat('d F Y') }}</td>
+      <td>{{ $data->posisi }}</td>
+      <td>
+        <a href="/view_pdf/{{ $data->id_biodata }}" class="btn btn-sm btn-primary">Detail</a>
+        <a href="/biodataupdateadmin/{{ $data->id_biodata }}" class="btn btn-sm btn-warning">Edit</a>
+        <button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#delete{{ $data->id_biodata }}">
+          Hapus
+        </button>
+      </td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
+
+@foreach ($biodata as $data)
+<div class="modal fade" id="delete{{ $data->id_biodata }}">
+  <div class="modal-dialog modal-sm">
+    <div class="modal-content bg-danger">
+      <div class="modal-header">
+        <h4 class="modal-title">{{ $data->nama }}</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Yakin ingin menghapus {{ $data->nama }}</p>
+      </div>
+      <div class="modal-footer justify-content-between">
+        <button type="button" class="btn btn-sm btn-primary" data-dismiss="modal">Tidak</button>
+        <a href="/allbiodata/delete/{{ $data->id_biodata }}" class="btn btn-sm btn-warning">Ya</a>
+      </div>
+    </div>
+    <!-- /.modal-content -->
+  </div>
+  <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
+@endforeach
+@endsection
---
+++ View/v_biodata.blade.php
@@ -0,0 +1,6 @@
+@extends('biodata.tab')
+@section('title','Biodata')
+@section('biodata')
+<a class="btn btn-sm btn-primary" href="/biodata/tambah">Update Biodata</a>
+<h2 style="align: center;">Silahkan isi Biodata anda dengan menekan tombol Update Biodata</h2>
+@endsection
